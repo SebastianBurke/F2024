42d99deae71f86e2dbea5de87521622df7f87e12
Canales Burke <2026996@cegep-heritage.qc.ca>
2024-08-23 15:02:11 -0400

    Added labs folder

---
+++ Web IV/Labs/L01/scb_H60L01/scb_H60L01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace scb_H60L01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Web IV/Labs/L01/scb_H60L01/scb_H60L01/Models/Person.cs
@@ -0,0 +1,53 @@
+﻿namespace scb_H60L01.Models
+{
+    public class Person
+    {
+        private static long CurrentPersonNum = 1;
+        private static List<Person> PersonList = new List<Person>();
+
+        public long PersonId;
+        public string FirstName;
+        public string LastName;
+        public string Country;
+        public bool Validated;
+        public string Email;
+
+        public Person (long personId, string firstName, string lastName, string country, bool validated, string email)
+        {
+            PersonId = personId;
+            FirstName = firstName;
+            LastName = lastName;
+            Country = country;
+            Validated = validated;
+            Email = email;
+        }
+        public Person(string firstName, string lastName, string country, bool validated, string email)
+        {
+            PersonId = CurrentPersonNum++;
+            FirstName = firstName;
+            LastName = lastName;
+            Country = country;
+            Validated = validated;
+            Email = email;
+        }
+
+        public Person (long personId)
+        {
+            PersonId = personId;
+        }
+
+        public Person () { }
+
+        public List<Person> GetPersons()
+        {
+            return PersonList;
+        }
+
+        public bool AddPerson()
+        {
+            PersonList.Add(this);
+            return true;
+        }
+
+    }
+}
