8838d9146002e5afaa69ca01248d98f1e280af28
2026996 <sebastian@ipf-mail.com>
2024-09-23 15:49:57 -0400

    KL05 start

---
+++ Web VI/Labs/L05/PartA/PartA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PartA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
